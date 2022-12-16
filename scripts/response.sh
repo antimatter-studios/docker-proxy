72fae665d9a3cda4d280cec6d87c8630360ad2ec
Christopher Thomas <chris.thomas@antimatter-studios.com>
2022-12-16 09:09:36 +0100

    handle the ability to reset the template back to the default configuration when no configurations are found in the docker system

--- scripts/response.sh
+++ scripts/response.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -e
 
-echo "Writing Configuration"
-echo "$(echo $1 | base64 -d)" > /etc/nginx/conf.d/default.conf
+config=/etc/nginx/conf.d/default.conf
+template="$(echo $1 | base64 -d)"
+
+if [ ! -z "${template}" ]; then
+    echo "Writing New Configuration"
+    echo "${template}" > ${config}
+else
+    echo "Resetting Configuration back to default"
+    cp /app/default.conf ${config}
+fi
 
 echo "Reloading NGINX"
 nginx -s reload
